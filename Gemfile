828319bd6082e9cd410d7974655b4dac969e7315
Joe Mahoney <jmahoney@iterative.co.nz>
2020-03-22 21:58:08 +1300

    Made it a sinatra app

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'nokogiri'
 gem 'httparty'
+gem 'sinatra'
--- wodfeed.rb
+++ wodfeed.rb
@@ -1,22 +1,43 @@
 require "nokogiri"
 require "httparty"
+require "sinatra"
+require "date"
 
-site = "https://www.crossfit.com"
-archive = "#{site}/workout"
+feed = []
+last_fetched = Date.today - 7
 
-response = HTTParty.get(archive)
+get "/" do
+  if last_fetched < DateTime.now - 0.5
+    feed = fetch
+    last_fetched = DateTime.now
+  end
+  feed.to_s
+end
 
-if response.success?
-  doc = Nokogiri.HTML(response.body)
-  rows = doc.css("div[class='row content-container']")
-  rows.each do |row|
-    h = row.css("h3[class='show']").first
-    permalink="#{site}#{h.children.first['href']}"
-    html = h.to_html
-    html << row.css("div[class='col-sm-6']").css("p").to_html
+def fetch
+  response = HTTParty.get("https://www.crossfit.com/workout")
 
-    puts permalink
-    puts html
-    puts "------------------------------------------------"
+  if response.success?
+    doc = Nokogiri.HTML(response.body)
+
+    feed = []
+
+    rows = doc.css("div[class='row content-container']")
+
+    rows.each do |row|
+      header = row.css("h3[class='show']").first
+      permalink="https://www.crossfit.com#{header.children.first['href']}"
+      title = header.text
+      html = header.to_html
+      html << row.css("div[class='col-sm-6']").css("p").to_html
+      feed << {id: permalink, content_html: html, title: title}
+    end
+
+    return feed
+
   end
 end
+
+
+
+
